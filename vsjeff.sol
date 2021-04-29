49ca45066dfde92a242c6ae15f1b841f5772271f
JeffxElon <83404869+JeffxElon@users.noreply.github.com>
2021-04-29 15:02:41 +0200

    vsjeff.sol

--- vsjeff.sol
+++ vsjeff.sol
@@ -48,7 +48,7 @@ interface IERC20 {
      */
     function approve(address spender, uint256 amount) external returns (bool);
 
-    /**
+    /** 
      * @dev Moves `amount` tokens from `sender` to `recipient` using the
      * allowance mechanism. `amount` is then deducted from the caller's
      * allowance.
